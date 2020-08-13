c8a6a2e6911d13078b27a6ff43ef5a5691570a39
mtcolvard <matthew.colvard@gmail.com>
2020-08-13 02:08:16 +0200

    changed the homing algo to look always at distance minus platonic width.  it much improved the pride of spitalfields to buckingham route but the pride to 42 stepney is worse than normal with extranious waypoints and not most direct route.  I suspect that the alleyway_bias is still not registering.

--- foxymaps/route_calculations/holming_algo_temp.py
+++ foxymaps/route_calculations/holming_algo_temp.py
@@ -1,29 +1,22 @@
+
+
 def run_homing_algo(all_waypoints):
     # create a graph with nodes for each waypoint
     waypoints_lon_lat = {k:[v['lon_lat'],v['size_in_hectares']] for k, v in all_waypoints.items()}
     waypoint_ids = list(all_waypoints.keys())
     waypoints_graph = {}
-    waypoints_graphed = {}
     for waypoint_id in waypoint_ids:
         waypoints_graph[waypoint_id] = {}
-        waypoints_graphed[waypoint_id] = {}
         for each_id in waypoint_ids:
             # populate every waypoint node [waypoint_id] with a list of the distance and angle to all other waypoints [each_id]
-            # crowflys_bearing_and_platonic_width =
-            # waypoints_graph[waypoint_id][each_id] = crowflys_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], math.sqrt(waypoints_lon_lat[each_id][1]*10000))
-            waypoints_graph[waypoint_id][each_id] = crowflys_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], math.sqrt(waypoints_lon_lat[each_id][1]*10000))
+            waypoints_graph[waypoint_id][each_id] = crowflys_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], waypoints_lon_lat[each_id][1])
 
-            crowflys_bearing_and_platonic_width = crowflys_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], math.sqrt(waypoints_lon_lat[each_id][1]*10000))
-
-            waypoints_graphed[waypoint_id][each_id] = {'crowflys': crowflys_bearing_and_platonic_width[0], 'bearing': crowflys_bearing_and_platonic_width[1], 'platonic_width': crowflys_bearing_and_platonic_width[2]}
     print('waypoints graph', waypoints_graph)
     print('waypoints graphed', waypoints_graphed)
     # Run filtering algorithm
     waypoint_route_order = filter_graph_by_angle_then_distance(waypoints_graph)
     print('waypoint_route_order', waypoint_route_order)
     return waypoint_route_order
-
-
 
 def filter_graph_by_angle_then_distance(waypoints_graph):
     route_order = ['origin']
@@ -38,8 +31,7 @@ def filter_graph_by_angle_then_distance(waypoints_graph):
         v[1] > (waypoints_graph[route_order[-1]]['destination'][1] - math.pi/4)}
     # Sort to find closest park
         # print('route_order', route_order)
-        print('angle_filtered_park_options', angle_filtered_park_options)
-
+        # print('angle_filtered_park_options', angle_filtered_park_options)
         # next_park = min(angle_filtered_park_options, key=lambda distance: angle_filtered_park_options[distance][0])
 
         parks_sorted_by_distance = sorted(angle_filtered_park_options, key=lambda distance:angle_filtered_park_options[distance][0])
@@ -56,18 +48,15 @@ def filter_graph_by_angle_then_distance(waypoints_graph):
             next_park = parks_sorted_by_distance[1]
         else:
             next_park = parks_sorted_by_distance[0]
-        # print('next park', next_park)
-        # # print('next_park_size', next_park_size)
-        # # print('closest_park_by_distance', closest_park_by_distance)
-        # # print('second_closest_park_by_distance', second_closest_park_by_distance)
-        print('closest park id and distance', [parks_sorted_by_distance[0], distance_to_closest_park])
-        # # print('distance_to_second_closest_park', distance_to_second_closest_park)
-        try:
-            second_closest_park_id = parks_sorted_by_distance[1]
-        except (IndexError):
-            second_closest_park_id = 'destination'
-        print('second_closest_park_id and platonic_width', [second_closest_park_id, platonic_width_of_second_closest_park_hecarage])
-        # # print('parks_sorted_by_distance', parks_sorted_by_distance)
+        # next_park_size = angle_filtered_park_options[parks_sorted_by_distance][0][]
+        print('next park', next_park)
+        # print('next_park_size', next_park_size)
+        # print('closest_park_by_distance', closest_park_by_distance)
+        # print('second_closest_park_by_distance', second_closest_park_by_distance)
+        # print('distance_to_closest_park', distance_to_closest_park)
+        # print('distance_to_second_closest_park', distance_to_second_closest_park)
+        # print('platonic_width_of_second_closest_park_hecarage', platonic_width_of_second_closest_park_hecarage)
+        # print('parks_sorted_by_distance', parks_sorted_by_distance)
 
         route_order.append(next_park)
     return route_order
--- foxymaps/route_calculations/homing_algo.py
+++ foxymaps/route_calculations/homing_algo.py
@@ -9,19 +9,21 @@ import math
 
 def run_homing_algo(all_waypoints):
     # create a graph with nodes for each waypoint
-    waypoints_lon_lat = {k:[v['lon_lat'],v['size_in_hectares']] for k, v in all_waypoints.items()}
+    # waypoints_lon_lat = {k:[v['lon_lat'],v['size_in_hectares']] for k, v in all_waypoints.items()}
     waypoint_ids = list(all_waypoints.keys())
     waypoints_graph = {}
-    waypoints_graphed = {}
     for waypoint_id in waypoint_ids:
         waypoints_graph[waypoint_id] = {}
-        waypoints_graphed[waypoint_id] = {}
         for each_id in waypoint_ids:
             # populate every waypoint node [waypoint_id] with a list of the distance and angle to all other waypoints [each_id]
-            waypoints_graph[waypoint_id][each_id] = crowflys_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], waypoints_lon_lat[each_id][1])
+            # crowflys_bearing_and_platonic_width = crowflys_distance_and_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], math.sqrt(waypoints_lon_lat[each_id][1]*10000))
+            crowflys_distance_bearing_and_platonic_width = crowflys_distance_and_bearing(all_waypoints[waypoint_id]['lon_lat'], all_waypoints[each_id]['lon_lat'], math.sqrt(all_waypoints[each_id]['size_in_hectares']*10000))
+            # waypoints_graph[waypoint_id][each_id] = crowflys_distance_and_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], math.sqrt(waypoints_lon_lat[each_id][1]*10000))
+            #
+            # crowflys_bearing_and_platonic_width = crowflys_distance_and_bearing(waypoints_lon_lat[waypoint_id][0], waypoints_lon_lat[each_id][0], math.sqrt(waypoints_lon_lat[each_id][1]*10000))
 
+            waypoints_graph[waypoint_id][each_id] = {'crowflys_distance': crowflys_distance_bearing_and_platonic_width[0], 'bearing': crowflys_distance_bearing_and_platonic_width[1], 'platonic_width': crowflys_distance_bearing_and_platonic_width[2], 'crowflys_distance_minus_platonic_width': (crowflys_distance_bearing_and_platonic_width[0]-crowflys_distance_bearing_and_platonic_width[2])}
     print('waypoints graph', waypoints_graph)
-    print('waypoints graphed', waypoints_graphed)
     # Run filtering algorithm
     waypoint_route_order = filter_graph_by_angle_then_distance(waypoints_graph)
     print('waypoint_route_order', waypoint_route_order)
@@ -35,43 +37,48 @@ def filter_graph_by_angle_then_distance(waypoints_graph):
             break
     # Filter out parks not within x degrees from each waypoint to the destination
         angle_filtered_park_options = {k:v for k, v in waypoints_graph[route_order[-1]].items() if
-        v[1] != 0 and
-        v[1] < (waypoints_graph[route_order[-1]]['destination'][1] + math.pi/4) and
-        v[1] > (waypoints_graph[route_order[-1]]['destination'][1] - math.pi/4)}
+        v['bearing'] != 0 and
+        v['bearing'] < (waypoints_graph[route_order[-1]]['destination']['bearing'] + math.pi/5) and
+        v['bearing'] > (waypoints_graph[route_order[-1]]['destination']['bearing'] - math.pi/5)}
     # Sort to find closest park
         # print('route_order', route_order)
-        # print('angle_filtered_park_options', angle_filtered_park_options)
-        # next_park = min(angle_filtered_park_options, key=lambda distance: angle_filtered_park_options[distance][0])
+        print('angle_filtered_park_options', angle_filtered_park_options)
 
-        parks_sorted_by_distance = sorted(angle_filtered_park_options, key=lambda distance:angle_filtered_park_options[distance][0])
-        closest_park_by_distance = angle_filtered_park_options[parks_sorted_by_distance[0]]
-        if len(parks_sorted_by_distance) > 1:
-            second_closest_park_by_distance = angle_filtered_park_options[parks_sorted_by_distance[1]]
-        else:
-            infinity = float('inf')
-            second_closest_park_by_distance = (infinity, 0, 0)
-        platonic_width_of_second_closest_park_hecarage = math.sqrt(second_closest_park_by_distance[2]*10000)
-        distance_to_closest_park = closest_park_by_distance[0]
-        distance_to_second_closest_park = second_closest_park_by_distance[0]
-        if (distance_to_second_closest_park - platonic_width_of_second_closest_park_hecarage) < distance_to_closest_park:
-            next_park = parks_sorted_by_distance[1]
-        else:
-            next_park = parks_sorted_by_distance[0]
-        # next_park_size = angle_filtered_park_options[parks_sorted_by_distance][0][]
-        print('next park', next_park)
-        # print('next_park_size', next_park_size)
-        # print('closest_park_by_distance', closest_park_by_distance)
-        # print('second_closest_park_by_distance', second_closest_park_by_distance)
-        # print('distance_to_closest_park', distance_to_closest_park)
-        # print('distance_to_second_closest_park', distance_to_second_closest_park)
-        # print('platonic_width_of_second_closest_park_hecarage', platonic_width_of_second_closest_park_hecarage)
-        # print('parks_sorted_by_distance', parks_sorted_by_distance)
+        # next_park = min(angle_filtered_park_options, key=lambda distance: angle_filtered_park_options[distance]['crowflys_distance'])
+        next_park = min(angle_filtered_park_options, key=lambda distance: angle_filtered_park_options[distance]['crowflys_distance_minus_platonic_width'])
+
+        # parks_sorted_by_distance = sorted(angle_filtered_park_options, key=lambda distance:angle_filtered_park_options[distance]['crowflys_distance'])
+        # closest_park_by_distance = angle_filtered_park_options[parks_sorted_by_distance[0]]
+        # if len(parks_sorted_by_distance) > 1:
+        #     second_closest_park_by_distance = angle_filtered_park_options[parks_sorted_by_distance[1]]
+        # else:
+        #     infinity = float('inf')
+        #     second_closest_park_by_distance = (infinity, 0, 0)
+        # platonic_width_of_second_closest_park_hecarage = math.sqrt(second_closest_park_by_distance[2]*10000)
+        # distance_to_closest_park = closest_park_by_distance[0]
+        # distance_to_second_closest_park = second_closest_park_by_distance[0]
+        # if (distance_to_second_closest_park - platonic_width_of_second_closest_park_hecarage) < distance_to_closest_park:
+        #     next_park = parks_sorted_by_distance[1]
+        # else:
+        #     next_park = parks_sorted_by_distance[0]
+        # # print('next park', next_park)
+        # # # print('next_park_size', next_park_size)
+        # # # print('closest_park_by_distance', closest_park_by_distance)
+        # # # print('second_closest_park_by_distance', second_closest_park_by_distance)
+        # print('closest park id and distance', [parks_sorted_by_distance[0], distance_to_closest_park])
+        # # # print('distance_to_second_closest_park', distance_to_second_closest_park)
+        # try:
+        #     second_closest_park_id = parks_sorted_by_distance[1]
+        # except (IndexError):
+        #     second_closest_park_id = 'destination'
+        # print('second_closest_park_id and platonic_width', [second_closest_park_id, platonic_width_of_second_closest_park_hecarage])
+        # # # print('parks_sorted_by_distance', parks_sorted_by_distance)
 
         route_order.append(next_park)
     return route_order
 
 
-def crowflys_bearing(startpoint, endpoint, size_in_hectares):
+def crowflys_distance_and_bearing(startpoint, endpoint, size_in_hectares):
     R = 6371000
     startpoint_lon = startpoint[0]
     startpoint_lat = startpoint[1]
@@ -84,9 +91,9 @@ def crowflys_bearing(startpoint, endpoint, size_in_hectares):
 # CROWFLYS
     a = math.sin(Δφ/2) * math.sin(Δφ/2) + math.cos(φ1) * math.cos(φ2) * math.sin(Δλ/2) * math.sin(Δλ/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
-    crowflys = R * c
+    crowflys_distance = R * c
 # BEARING
     y = math.sin(Δλ) * math.cos(φ2)
     x = math.cos(φ1)*math.sin(φ2) - math.sin(φ1)*math.cos(φ2)*math.cos(Δλ)
     θ = math.atan2(y, x)
-    return crowflys, θ, size_in_hectares
+    return crowflys_distance, θ, size_in_hectares
