1bbcbb5394b867c1b080cec77997188c09fc7adc
mtcolvard <matthew.colvard@gmail.com>
2020-09-28 22:20:04 +0200

    set up settings.base.py

--- project/urls.py
+++ project/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
-    # path('auth/', include('jwt_auth.urls')),
     path('admin/', include('rest_framework.urls')),
+    path('api/', include('jwt_auth.urls')),
     path('api/', include('foxymaps.urls')),
     path('', include('frontend.urls'))
 
