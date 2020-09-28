a82631858894aa5cb05d739edea5887652afc947
mtcolvard <matthew.colvard@gmail.com>
2020-09-28 23:58:00 +0200

    deployed to heroku. backend working. still having problems with bundle.js on the frontend

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const Dotenv = require('dotenv-webpack')
-
+ 
 
 module.exports = {
   entry: './src/app.js',
