a604c4b75284174a52b3712400ea63eeb9cdba60
mtcolvard <matthew.colvard@gmail.com>
2020-10-04 21:31:36 +0200

    changed template_view from bundle.js to index.html

--- frontend/urls.py
+++ frontend/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', Home.as_view(), name='home'),
     # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(), name='assets'),
     # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(content_type='text/javascript'), name='assets'),
-    re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(template_name='bundle.js', content_type='text/javascript'), name='assets'),
+    re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(template_name='index.html', content_type='text/javascript'), name='assets'),
     # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(content_type='text/javascript'), name='assets'),
     # re_path(r'^.*', TemplateView.as_view(
     #     template_name='index.html',
