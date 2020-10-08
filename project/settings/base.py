56971e82e7a82d6ae60719e801a16a25e94ad248
mtcolvard <matthew.colvard@gmail.com>
2020-10-08 23:46:38 +0200

    changed filed directory to os.path.join(BASE_DIR, '../frontend/dist')

--- project/settings/base.py
+++ project/settings/base.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist')
+    os.path.join(BASE_DIR, '../frontend/dist')
 ]
 
 REST_FRAMEWORK = {
