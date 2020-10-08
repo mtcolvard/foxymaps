14f47d39a90d02cfbece92cfc98ce305e24bdf15
mtcolvard <matthew.colvard@gmail.com>
2020-10-09 00:29:45 +0200

    forgot to comment Assets back in

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, re_path
 from django.views.generic import TemplateView
-from .views import Home
+from .views import Home, Assets
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(), name='assets'),
+    re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(), name='assets'),
     # re_path('.*', TemplateView.as_view(template_name='index.html'))
     # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(template_name='index.html'))
     # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(content_type='text/javascript'), name='assets'),
