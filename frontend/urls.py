c76a4dd31c4101b6c874640c1f1361380a47e784
mtcolvard <matthew.colvard@gmail.com>
2020-10-19 23:39:15 +0200

    discarding commented out code from frontend/urls

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,30 +5,4 @@ from .views import Home, Assets
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(), name='assets'),
-    # re_path('.*', TemplateView.as_view(template_name='index.html'))
-    # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(template_name='index.html'))
-    # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(content_type='text/javascript'), name='assets'),
-    # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(template_name='index.html', content_type='text/javascript'), name='assets'),
-    # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(content_type='text/javascript'), name='assets'),
-    # re_path(r'^.*', TemplateView.as_view(
-    #     template_name='index.html',
-    #     content_type='text/html'))
 ]
-
-
-
-
-# from django.urls import path, re_path
-# from .views import Home, Assets
-# from django.views.generic import TemplateView
-#
-# urlpatterns = [
-#     path('', Home.as_view(), name='home'),
-#     re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(), name='assets'),
-#     # re_path(r'^(?P<filename>[\w\.]+)$', Assets.as_view(content_type='text/javascript'), name='assets'),
-#     # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(template_name='index.html', content_type='text/javascript'), name='assets'),
-#     # re_path(r'^(?P<filename>[\w\.]+)$', TemplateView.as_view(content_type='text/javascript'), name='assets'),
-#     # re_path(r'^.*', TemplateView.as_view(
-#     #     template_name='index.html',
-#     #     content_type='text/html'))
-# ]
