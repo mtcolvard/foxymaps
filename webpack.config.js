119f3ed526ab9b151ef247fc06c84a3fa0897a3e
mtcolvard <matthew.colvard@gmail.com>
2020-10-24 01:14:13 +0200

    removed unused import from frontend/urls.py

--- webpack.config.js
+++ webpack.config.js
@@ -48,11 +48,7 @@ module.exports = {
 
 
 //
-// const path = require('path')
-// const webpack = require('webpack')
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
-// // const LinkTypePlugin = require('html-webpack-link-type-plugin').HtmlWebpackLinkTypePlugin
-// const Dotenv = require('dotenv-webpack')
+// const LinkTypePlugin = require('html-webpack-link-type-plugin').HtmlWebpackLinkTypePlugin
 // const minifyOptions = {
 //                 collapseWhitespace: true,
 //                 removeComments: true,
@@ -61,9 +57,6 @@ module.exports = {
 //                 // removeStyleLinkTypeAttributes: false,
 //                 // removeScriptTypeAttributes: false
 //             }
-//
-//
-//
 // module.exports = {
 //   entry: './src/app.js',
 //   context: path.resolve(__dirname, 'frontend'),
@@ -72,25 +65,6 @@ module.exports = {
 //     path: path.resolve(__dirname, 'frontend/dist')
 //   },
 //   devtool: 'source-maps',
-//   module: {
-//     rules: [
-//       { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
-//       { test: /\.css$/, loader: ['style-loader', 'css-loader'] },
-//       { test: /\.s(a|c)ss$/, loader: ['style-loader', 'css-loader', 'sass-loader'] },
-//       { test: /\.woff2?$/, loader: 'file-loader' },
-//       { test: /\.(jpg|png|gif)$/, loader: 'file-loader' }
-//     ]
-//   },
-//   devServer: {
-//     contentBase: path.join(__dirname, 'frontend/src'),
-//     hot: true,
-//     open: true,
-//     port: 8000,
-//     watchContentBase: true,
-//     proxy: {
-//       '/api': 'http://localhost:4000'
-//     }
-//   },
 //   plugins: [
 //     new Dotenv({
 //       path: path.resolve(__dirname, './.env'),
