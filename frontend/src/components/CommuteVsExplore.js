99df008a7d9f42f568ff45efc6a54b38668b85cb
mtcolvard <matthew.colvard@gmail.com>
2022-09-21 15:18:21 +0200

    began upgrade to yarn 3

--- frontend/src/components/CommuteVsExplore.js
+++ frontend/src/components/CommuteVsExplore.js
@@ -13,21 +13,21 @@ export default class CommuteVsExplore extends React.Component {
   render() {
     const toggleExplore = this.props.toggleExplore
     return(
-        <div className="commuteExploreField">
-          <div className="level levelCommuteExplore is-mobile">
-            <div className="level-item has-text-centered">
-              <div>
-                <p className="pCommuteExplore">Explore</p>
-              </div>
+      <div className="commuteExploreField">
+        <div className="level levelCommuteExplore is-mobile">
+          <div className="level-item has-text-centered">
+            <div>
+              <p className="pCommuteExplore">Explore</p>
             </div>
-            <div className="level-item has-text-centered">
-              <div>
-                <input type="checkbox" id="toggle" className="checkbox" onClick={this.handleToggleClick} />
-                <label htmlFor="toggle" className="switch"></label>
-              </div>
+          </div>
+          <div className="level-item has-text-centered">
+            <div>
+              <input type="checkbox" id="toggle" className="checkbox" onClick={this.handleToggleClick} />
+              <label htmlFor="toggle" className="switch"></label>
             </div>
           </div>
         </div>
+      </div>
     )
   }
 }
